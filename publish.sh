b4e0352f60cbe48e6df585fd2c7f2df90196325f
Alex White <alex.white@hey.com>
2025-11-23 09:04:53 -0500

    Publishing

--- publish.sh
+++ publish.sh
@@ -5,5 +5,11 @@ git commit -am "Publishing"
 git push
 
 npx @11ty/eleventy
-cd _site
-xmit thatalexguy.dev
+#cd _site
+#xmit thatalexguy.dev
+
+echo "⛾ Deploying, grab a coffee"
+
+rsync -az --checksum --delete --omit-dir-times --quiet "./_site" "root@159.89.45.162:/var/www/thatalexguy.dev/"
+
+echo "✅ Deployed"
