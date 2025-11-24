4606bb95c1e37485f4302b832ab1cceae853a18c
Alex White <alex.white@hey.com>
2025-11-23 22:08:01 -0500

    Publishing

--- publish.sh
+++ publish.sh
@@ -14,6 +14,6 @@ npx @11ty/eleventy
 
 echo "⛾ Deploying, grab a coffee"
 
-rsync -az --checksum --omit-dir-times --quiet "./_site/" "root@159.89.45.162:/var/www/thatalexguy.dev/"
-
+rsync -az --checksum --omit-dir-times --quiet "./_site/" "root@thatalexguy.dev:/var/www/thatalexguy.dev/"
+ssh root@thatalexguy.dev -t 'sudo chown www-data:www-data /var/www/thatalexguy.dev/ -R'
 echo "✅ Deployed"
